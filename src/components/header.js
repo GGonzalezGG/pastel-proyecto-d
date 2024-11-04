ccb80aa778c444811089dbd1973fa2353cf159ae
blame1617 <xkiritox1995@gmail.com>
2024-11-04 00:13:49 -0300

    Agregado Local Storage en Home page

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,17 @@
+// components/Header.js
 import Link from 'next/link';
 import { useContext } from 'react';
-import { LanguageContext } from '../pages/_app';
+import { LanguageContext } from '../pages/_app'; // Asegúrate de que esta ruta sea correcta
+import { useAuth } from '../app/context/AuthContext'; // Ajusta la ruta según tu estructura
 
 export default function Header() {
     const { t } = useContext(LanguageContext);
+    const { user } = useAuth(); // Obtén el usuario desde el AuthContext
+
     return (
         <header className="flex flex-col md:flex-row justify-between items-center p-4 bg-gray-900 bg-opacity-90">
             <div className="flex-grow text-center">
-                <h1 className="text-3xl md:text-4xl">{t("welcome")}</h1>
+                <h1 className="text-3xl md:text-4xl">{user ? `Bienvenido ${user.username}` : t("welcome")}</h1>
             </div>
             <div className="flex-none space-x-2 mt-2 md:mt-0">
                 <Link href="/login">
